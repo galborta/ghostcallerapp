d96210813af906d7be4ac6f1fa7ddd12f5880a76
galborta <galborta@protonmail.com>
2025-04-25 15:41:21 -0400

    feat: add Artist model with freezed annotations and tests

---
+++ lib/data/models/artist_model.freezed.dart
@@ -0,0 +1,383 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'artist_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Artist _$ArtistFromJson(Map<String, dynamic> json) {
+  return _Artist.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Artist {
+  String get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get bio => throw _privateConstructorUsedError;
+  @JsonKey(name: 'short_bio')
+  String get shortBio => throw _privateConstructorUsedError;
+  @JsonKey(name: 'image_url')
+  String get imageUrl => throw _privateConstructorUsedError;
+  bool get featured => throw _privateConstructorUsedError;
+  @JsonKey(name: 'revenue_share_percentage')
+  double get revenueSharePercentage => throw _privateConstructorUsedError;
+  @JsonKey(name: 'referral_code')
+  String get referralCode => throw _privateConstructorUsedError;
+  @JsonKey(name: 'created_at')
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'updated_at')
+  DateTime get updatedAt => throw _privateConstructorUsedError;
+
+  /// Serializes this Artist to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Artist
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ArtistCopyWith<Artist> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ArtistCopyWith<$Res> {
+  factory $ArtistCopyWith(Artist value, $Res Function(Artist) then) =
+      _$ArtistCopyWithImpl<$Res, Artist>;
+  @useResult
+  $Res call(
+      {String id,
+      String name,
+      String bio,
+      @JsonKey(name: 'short_bio') String shortBio,
+      @JsonKey(name: 'image_url') String imageUrl,
+      bool featured,
+      @JsonKey(name: 'revenue_share_percentage') double revenueSharePercentage,
+      @JsonKey(name: 'referral_code') String referralCode,
+      @JsonKey(name: 'created_at') DateTime createdAt,
+      @JsonKey(name: 'updated_at') DateTime updatedAt});
+}
+
+/// @nodoc
+class _$ArtistCopyWithImpl<$Res, $Val extends Artist>
+    implements $ArtistCopyWith<$Res> {
+  _$ArtistCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Artist
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? bio = null,
+    Object? shortBio = null,
+    Object? imageUrl = null,
+    Object? featured = null,
+    Object? revenueSharePercentage = null,
+    Object? referralCode = null,
+    Object? createdAt = null,
+    Object? updatedAt = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      bio: null == bio
+          ? _value.bio
+          : bio // ignore: cast_nullable_to_non_nullable
+              as String,
+      shortBio: null == shortBio
+          ? _value.shortBio
+          : shortBio // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      featured: null == featured
+          ? _value.featured
+          : featured // ignore: cast_nullable_to_non_nullable
+              as bool,
+      revenueSharePercentage: null == revenueSharePercentage
+          ? _value.revenueSharePercentage
+          : revenueSharePercentage // ignore: cast_nullable_to_non_nullable
+              as double,
+      referralCode: null == referralCode
+          ? _value.referralCode
+          : referralCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ArtistImplCopyWith<$Res> implements $ArtistCopyWith<$Res> {
+  factory _$$ArtistImplCopyWith(
+          _$ArtistImpl value, $Res Function(_$ArtistImpl) then) =
+      __$$ArtistImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String name,
+      String bio,
+      @JsonKey(name: 'short_bio') String shortBio,
+      @JsonKey(name: 'image_url') String imageUrl,
+      bool featured,
+      @JsonKey(name: 'revenue_share_percentage') double revenueSharePercentage,
+      @JsonKey(name: 'referral_code') String referralCode,
+      @JsonKey(name: 'created_at') DateTime createdAt,
+      @JsonKey(name: 'updated_at') DateTime updatedAt});
+}
+
+/// @nodoc
+class __$$ArtistImplCopyWithImpl<$Res>
+    extends _$ArtistCopyWithImpl<$Res, _$ArtistImpl>
+    implements _$$ArtistImplCopyWith<$Res> {
+  __$$ArtistImplCopyWithImpl(
+      _$ArtistImpl _value, $Res Function(_$ArtistImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Artist
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? bio = null,
+    Object? shortBio = null,
+    Object? imageUrl = null,
+    Object? featured = null,
+    Object? revenueSharePercentage = null,
+    Object? referralCode = null,
+    Object? createdAt = null,
+    Object? updatedAt = null,
+  }) {
+    return _then(_$ArtistImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      bio: null == bio
+          ? _value.bio
+          : bio // ignore: cast_nullable_to_non_nullable
+              as String,
+      shortBio: null == shortBio
+          ? _value.shortBio
+          : shortBio // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      featured: null == featured
+          ? _value.featured
+          : featured // ignore: cast_nullable_to_non_nullable
+              as bool,
+      revenueSharePercentage: null == revenueSharePercentage
+          ? _value.revenueSharePercentage
+          : revenueSharePercentage // ignore: cast_nullable_to_non_nullable
+              as double,
+      referralCode: null == referralCode
+          ? _value.referralCode
+          : referralCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ArtistImpl implements _Artist {
+  const _$ArtistImpl(
+      {required this.id,
+      required this.name,
+      required this.bio,
+      @JsonKey(name: 'short_bio') required this.shortBio,
+      @JsonKey(name: 'image_url') required this.imageUrl,
+      required this.featured,
+      @JsonKey(name: 'revenue_share_percentage')
+      required this.revenueSharePercentage,
+      @JsonKey(name: 'referral_code') required this.referralCode,
+      @JsonKey(name: 'created_at') required this.createdAt,
+      @JsonKey(name: 'updated_at') required this.updatedAt});
+
+  factory _$ArtistImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ArtistImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String name;
+  @override
+  final String bio;
+  @override
+  @JsonKey(name: 'short_bio')
+  final String shortBio;
+  @override
+  @JsonKey(name: 'image_url')
+  final String imageUrl;
+  @override
+  final bool featured;
+  @override
+  @JsonKey(name: 'revenue_share_percentage')
+  final double revenueSharePercentage;
+  @override
+  @JsonKey(name: 'referral_code')
+  final String referralCode;
+  @override
+  @JsonKey(name: 'created_at')
+  final DateTime createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  final DateTime updatedAt;
+
+  @override
+  String toString() {
+    return 'Artist(id: $id, name: $name, bio: $bio, shortBio: $shortBio, imageUrl: $imageUrl, featured: $featured, revenueSharePercentage: $revenueSharePercentage, referralCode: $referralCode, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ArtistImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.bio, bio) || other.bio == bio) &&
+            (identical(other.shortBio, shortBio) ||
+                other.shortBio == shortBio) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.featured, featured) ||
+                other.featured == featured) &&
+            (identical(other.revenueSharePercentage, revenueSharePercentage) ||
+                other.revenueSharePercentage == revenueSharePercentage) &&
+            (identical(other.referralCode, referralCode) ||
+                other.referralCode == referralCode) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      name,
+      bio,
+      shortBio,
+      imageUrl,
+      featured,
+      revenueSharePercentage,
+      referralCode,
+      createdAt,
+      updatedAt);
+
+  /// Create a copy of Artist
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ArtistImplCopyWith<_$ArtistImpl> get copyWith =>
+      __$$ArtistImplCopyWithImpl<_$ArtistImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ArtistImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Artist implements Artist {
+  const factory _Artist(
+          {required final String id,
+          required final String name,
+          required final String bio,
+          @JsonKey(name: 'short_bio') required final String shortBio,
+          @JsonKey(name: 'image_url') required final String imageUrl,
+          required final bool featured,
+          @JsonKey(name: 'revenue_share_percentage')
+          required final double revenueSharePercentage,
+          @JsonKey(name: 'referral_code') required final String referralCode,
+          @JsonKey(name: 'created_at') required final DateTime createdAt,
+          @JsonKey(name: 'updated_at') required final DateTime updatedAt}) =
+      _$ArtistImpl;
+
+  factory _Artist.fromJson(Map<String, dynamic> json) = _$ArtistImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get name;
+  @override
+  String get bio;
+  @override
+  @JsonKey(name: 'short_bio')
+  String get shortBio;
+  @override
+  @JsonKey(name: 'image_url')
+  String get imageUrl;
+  @override
+  bool get featured;
+  @override
+  @JsonKey(name: 'revenue_share_percentage')
+  double get revenueSharePercentage;
+  @override
+  @JsonKey(name: 'referral_code')
+  String get referralCode;
+  @override
+  @JsonKey(name: 'created_at')
+  DateTime get createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  DateTime get updatedAt;
+
+  /// Create a copy of Artist
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ArtistImplCopyWith<_$ArtistImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
